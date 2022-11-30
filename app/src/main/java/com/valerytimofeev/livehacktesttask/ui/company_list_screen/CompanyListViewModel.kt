d9ae858ccb1e68fa21b40b490c57421e8e016810
Haartag <90470991+Haartag@users.noreply.github.com>
2022-11-30 22:53:19 +0300

    Added CompanyDetailsScreen and ViewModel, added new colors for UI

---
+++ app/src/main/java/com/valerytimofeev/livehacktesttask/ui/company_list_screen/CompanyListViewModel.kt
@@ -0,0 +1,37 @@
+package com.valerytimofeev.livehacktesttask.ui.company_list_screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.valerytimofeev.livehacktesttask.data.remote.responses.LifeHackResponse
+import com.valerytimofeev.livehacktesttask.ui.theme.LoadingAnimation
+import com.valerytimofeev.livehacktesttask.ui.theme.LoadingAnimationLight
+import com.valerytimofeev.livehacktesttask.usecase.LoadCompanyListUseCase
+import com.valerytimofeev.livehacktesttask.utility.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CompanyListViewModel @Inject constructor(
+    private val loadCompanyList: LoadCompanyListUseCase
+) : ViewModel() {
+
+    val loadingAnimationColor = LoadingAnimation
+    val loadingAnimationLightColor = LoadingAnimationLight
+
+    /**
+     * Data from API call - [LifeHackResponse]
+     */
+    private val _remoteData =
+        MutableStateFlow(Resource.success(LifeHackResponse()))
+    val remoteData: StateFlow<Resource<LifeHackResponse>> = _remoteData
+
+    init {
+        viewModelScope.launch {
+            loadCompanyList.loadCompanyList().collect {
+                _remoteData.value = it
+            }
+        }
+    }
+}
--- app/src/main/java/com/valerytimofeev/livehacktesttask/ui/theme/Color.kt
+++ app/src/main/java/com/valerytimofeev/livehacktesttask/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Surface = Color(0xFF505050)
+val TileDefaultBackground = Color(0xFF777777)
+val LoadingAnimation = Color(0xFFDB9752)
+val LoadingAnimationLight = Color(0xFFD6BB59)
