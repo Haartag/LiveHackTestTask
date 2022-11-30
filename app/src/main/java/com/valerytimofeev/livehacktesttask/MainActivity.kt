baccbfe20358261700c9849163ff9894e64b7c37
Haartag <90470991+Haartag@users.noreply.github.com>
2022-12-01 01:13:13 +0300

    Navigation

--- app/src/main/java/com/valerytimofeev/livehacktesttask/MainActivity.kt
+++ app/src/main/java/com/valerytimofeev/livehacktesttask/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.valerytimofeev.livehacktesttask.ui.theme.LiveHackTestTaskTheme
+import com.valerytimofeev.livehacktesttask.ui.theme.Surface
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +18,9 @@ class MainActivity : ComponentActivity() {
             LiveHackTestTaskTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Surface
                 ) {
-
+                    Navigation()
                 }
             }
         }
---
+++ app/src/main/java/com/valerytimofeev/livehacktesttask/Navigation.kt
@@ -0,0 +1,41 @@
+package com.valerytimofeev.livehacktesttask
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.valerytimofeev.livehacktesttask.ui.company_details_screen.CompanyDetailsScreen
+import com.valerytimofeev.livehacktesttask.ui.company_list_screen.CompanyListScreen
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "company_list"
+    ) {
+        composable("company_list") {
+            CompanyListScreen(navController = navController)
+        }
+        composable(
+            "company_list/{companyId}",
+            arguments = listOf(navArgument("companyId") {
+                type = NavType.IntType
+            })
+        ) {
+            val companyId = remember {
+                it.arguments?.getInt("companyId")
+            }
+            CompanyDetailsScreen(
+                navController = navController,
+                companyId = companyId ?: -1
+            )
+        }
+
+    }
+}
